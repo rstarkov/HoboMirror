709870ba6ecd58ba68b8fe909262e1e86110f634
Roman Starkov <sourcecode@roman.st>
2025-09-18 10:53:51 +0100

    Tweak log/status

--- Src/Program.cs
+++ Src/Program.cs
@@ -272,7 +272,7 @@ class Program
         while (true)
         {
             if (StatusText != null)
-                Console.Title = StatusText + " : " + DateTime.UtcNow.Millisecond;
+                Console.Title = StatusText;
             Thread.Sleep(50);
         }
     }
@@ -681,7 +681,7 @@ class Program
         var start = DateTime.UtcNow;
         var paths = TryCatchIo(() => Filesys.ListDirectory(path), err => $"Unable to list directory ({err}): {path}");
         var time = (DateTime.UtcNow - start).TotalSeconds;
-        if (time > 0.2)
+        if (time > 1.0)
             LogAction($"Enumerate directory: {time:0.00} seconds for {paths?.Count ?? -999:#,0} entries: {path}");
         if (paths == null)
             return null;
